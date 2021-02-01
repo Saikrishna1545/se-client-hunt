0ca8dfd5ea9f24d6fb6bb6c90af9548238d74ea8
SaiKrishna1545 <s538340@nwmissouri.edu>
2021-02-01 13:23:00 -0600

    updated location

--- init-locations.js
+++ init-locations.js
@@ -1,8 +1,8 @@
 export default [
     {
         Name: '1114 easten Avenue Apartments, somerset, new jersey',
-        Latitude: 40.517155, 
-        Longitude: -74.491381,
+        Latitude: 40.517632, 
+        Longitude: -74.488218,
        
     },
     
